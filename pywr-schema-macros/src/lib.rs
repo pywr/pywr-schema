e4ff9f18d99397f996c8e5e78fbfb42fc4f4f99b
James Tomlinson <tomo.bbe@gmail.com>
2023-10-12 16:05:11 +0100

    chore: Add additional comments to pywr-schema-macros

--- pywr-schema-macros/src/lib.rs
+++ pywr-schema-macros/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
 use quote::quote;
 
+/// A derive macro for Pywr nodes that implements `parameters` and `parameters_mut` methods.
 #[proc_macro_derive(PywrNode)]
 pub fn pywr_node_macro(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
@@ -8,6 +9,8 @@ pub fn pywr_node_macro(input: TokenStream) -> TokenStream {
     impl_parameter_references_derive(&input)
 }
 
+/// A derive macro for Pywr parameters that implements `parameters`, `parameters_mut`,
+/// `resource_paths` and `update_resource_paths` methods.
 #[proc_macro_derive(PywrParameter)]
 pub fn pywr_parameter_macro(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
@@ -32,31 +35,38 @@ fn impl_parameter_references_derive(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
 
     if let syn::Data::Struct(data) = &ast.data {
+        // Only apply this to structs
+
+        // Help struct for capturing parameter fields and whether they are optional.
         struct ParamField {
             field_name: syn::Ident,
             optional: bool,
         }
 
+        // Iterate through all fields of the struct. Try to find fields that reference
+        // parameters (e.g. `Option<ParameterValue>` or `ParameterValue`).
         let parameter_fields: Vec<ParamField> = data
             .fields
             .iter()
             .filter_map(|field| {
                 let field_ident = field.ident.as_ref()?;
-
+                // Identify optional fields
                 match type_to_ident(&field.ty) {
                     Some(PywrField::Optional(ident)) => {
+                        // If optional and a parameter identifier then add to the list
                         is_parameter_ident(&ident).then_some(ParamField {
                             field_name: field_ident.clone(),
                             optional: true,
                         })
                     }
                     Some(PywrField::Required(ident)) => {
+                        // Otherwise, if a parameter identifier then add to the list
                         is_parameter_ident(&ident).then_some(ParamField {
                             field_name: field_ident.clone(),
                             optional: false,
                         })
                     }
-                    None => None,
+                    None => None, // All other fields are ignored.
                 }
             })
             .collect();
@@ -82,7 +92,7 @@ fn impl_parameter_references_derive(ast: &syn::DeriveInput) -> TokenStream {
             })
             .collect::<Vec<_>>();
 
-        // Insert statements for non-mutable version
+        // Insert statements for mutable version
         let inserts_mut = parameter_fields
             .iter()
             .map(|param_field| {
@@ -131,12 +141,14 @@ fn impl_parameter_references_derive(ast: &syn::DeriveInput) -> TokenStream {
     }
 }
 
-/// Generates a [`TokenStream`] containing the implementation `resource_paths`.
+/// Generates a [`TokenStream`] containing the implementation `resource_paths`
+/// and `update_resource_paths` methods.
 fn impl_parameter_resource_paths_derive(ast: &syn::DeriveInput) -> TokenStream {
     // Name of the node type
     let name = &ast.ident;
 
     if let syn::Data::Struct(data) = &ast.data {
+        // Helper struct to capture PathBuf fields
         struct PathField {
             field_name: syn::Ident,
             ty: PathFieldType,
@@ -149,8 +161,10 @@ fn impl_parameter_resource_paths_derive(ast: &syn::DeriveInput) -> TokenStream {
             .filter_map(|field| {
                 let field_ident = field.ident.as_ref()?;
 
+                // Identify optional fields
                 match type_to_ident(&field.ty) {
                     Some(PywrField::Optional(ident)) => {
+                        // If optional and a path identifier then add to the list
                         ident_to_path_type(&ident).map(|field_type| PathField {
                             field_name: field_ident.clone(),
                             ty: field_type,
@@ -158,13 +172,14 @@ fn impl_parameter_resource_paths_derive(ast: &syn::DeriveInput) -> TokenStream {
                         })
                     }
                     Some(PywrField::Required(ident)) => {
+                        // If required, and a path identifier then add to the list
                         ident_to_path_type(&ident).map(|field_type| PathField {
                             field_name: field_ident.clone(),
                             ty: field_type,
                             optional: false,
                         })
                     }
-                    None => None,
+                    None => None, // All other field types are ignored
                 }
             })
             .collect();
@@ -206,6 +221,7 @@ fn impl_parameter_resource_paths_derive(ast: &syn::DeriveInput) -> TokenStream {
             })
             .collect::<Vec<_>>();
 
+        // Update statements for the `update_resource_paths` method
         let updates = path_fields
             .iter()
             .map(|param_field| {
@@ -284,22 +300,28 @@ enum PywrField {
 /// Returns the last segment of a type path as an identifier
 fn type_to_ident(ty: &syn::Type) -> Option<PywrField> {
     match ty {
+        // Match type's that are a path and not a self type.
         syn::Type::Path(type_path) if type_path.qself.is_none() => {
+            // Match on the last segment
             match type_path.path.segments.last() {
                 Some(last_segment) => {
                     let ident = &last_segment.ident;
 
                     if ident == "Option" {
+                        // The last segment is an Option, now we need to parse the argument
+                        // I.e. the bit in inside the angle brackets.
                         let first_arg = match &last_segment.arguments {
                             syn::PathArguments::AngleBracketed(params) => params.args.first(),
                             _ => None,
                         };
 
+                        // Find type arguments; ignore others
                         let arg_ty = match first_arg {
                             Some(syn::GenericArgument::Type(ty)) => Some(ty),
                             _ => None,
                         };
 
+                        // Match on path types that are no self types.
                         let arg_type_path = match arg_ty {
                             Some(ty) => match ty {
                                 syn::Type::Path(type_path) if type_path.qself.is_none() => {
@@ -310,11 +332,13 @@ fn type_to_ident(ty: &syn::Type) -> Option<PywrField> {
                             None => None,
                         };
 
+                        // Get the last segment of the path
                         let last_segment = match arg_type_path {
                             Some(type_path) => type_path.path.segments.last(),
                             None => None,
                         };
 
+                        // Finally, if there's a last segment return this as an optional `PywrField`
                         match last_segment {
                             Some(last_segment) => {
                                 let ident = &last_segment.ident;
@@ -323,6 +347,7 @@ fn type_to_ident(ty: &syn::Type) -> Option<PywrField> {
                             None => None,
                         }
                     } else {
+                        // Otherwise, assume this a simple required field
                         Some(PywrField::Required(ident.clone()))
                     }
                 }
